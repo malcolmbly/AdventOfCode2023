b81d5ac8c4bb72366b3871a91e3ca3ea94ca1407
Malcolm Bailey <malcolm.bailey@spiff.com>
2023-12-04 13:56:05 -0800

    Moved code around

--- Day1/decode_document.py
+++ Day1/decode_document.py
@@ -1,10 +1,14 @@
 import re
 
+def format_and_decode(filename):
+  print(decode(format_document(filename)))
+
 def decode(filename):
   file = open(filename)
   lines = file.readlines()
   first_number_pattern = re.compile('\\d')
   last_number_pattern = re.compile('\\d(?!\\S*\\d)')
+  
   sum = 0
   for line in lines:
     print(line)
@@ -29,22 +33,19 @@ def format_document(filename):
   "eight": "8",
   "nine": "9"
   }
+  pattern = re.compile('(one|two|three|four|five|six|seven|eight|nine)')
+
   unformatted_file = open(filename, 'r')
   formatted_filename = filename + "_formatted"
   formatted_file = open(formatted_filename, 'w')
   lines = unformatted_file.readlines()
-  pattern = re.compile('(one|two|three|four|five|six|seven|eight|nine)')
+
   for line in lines:
-    # search in line for each of the keys above
-    # find the one that has the lowest value for m.start
     while pattern.search(line) is not None:
       number_text_match_value = pattern.search(line).group()
       line = re.sub(number_text_match_value, string_to_number_dict[number_text_match_value], line)
     formatted_file.write(line)
 
   return formatted_filename
-
-def format_and_decode(filename):
-  print(decode(format_document(filename)))
 
 format_and_decode("strings")
